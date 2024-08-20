db2d00c3f02bce42e5e803f76533c0787c04f922
Benjamin Gauvin <benjamingauvin.dev@gmail.com>
2024-08-20 11:37:10 +0200

    change gallery order

--- my-mixo/src/components/Gallery/Gallery.js
+++ my-mixo/src/components/Gallery/Gallery.js
@@ -10,7 +10,6 @@ import verreDoseur from '../../assets/verre_doseur.jpg'
 
 export default function Gallery() {
     return <div className={styles.galleryContainer}>
-        <img src={cocktailOrange} loading="lazy" alt="Cocktail avec une tranche d'orange brûlée" className={styles.galleryPic} />
         <img src={desPaillettes} loading="lazy" alt="Cocktail avec des paillettes" className={styles.galleryPic} />
         <img src={experienceChimique} loading="lazy" alt="ça ressemble à une expérience chimique" className={styles.galleryPic} />
         <img src={laurierColada} loading="lazy" alt="Une pina colada avec une feuille de laurier" className={styles.galleryPic} />
@@ -18,5 +17,6 @@ export default function Gallery() {
         <img src={mentheCitron} loading="lazy" alt="Cocktail décoré avec menthe et citron" className={styles.galleryPic} />
         <img src={petitParasol} loading="lazy" alt="Cocktail décoré avec un petit parasol" className={styles.galleryPic} />
         <img src={verreDoseur} loading="lazy" alt="Cocktail à coté d'un verre doseur" className={styles.galleryPic} />
+        <img src={cocktailOrange} loading="lazy" alt="Cocktail avec une tranche d'orange brûlée" className={styles.galleryPic} />
     </div>
 }
