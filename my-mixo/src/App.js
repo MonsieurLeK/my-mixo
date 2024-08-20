c0279936e3268ff160f8b9767e6229c501371a7a
Benjamin Gauvin <benjamingauvin.dev@gmail.com>
2024-08-20 12:52:03 +0200

    Button menu

--- my-mixo/src/App.js
+++ my-mixo/src/App.js
@@ -1,12 +1,14 @@
 import './App.css'
 import Header from './components/Header/Header'
-import Gallery from './components/Gallery/Gallery';
+import Gallery from './components/Gallery/Gallery'
+import Modale from './components/Modale/Modale';
 
 function App() {
 
   return (
     <div className="App">
       <Header/>
+      <Modale />
       <Gallery />
     </div>
   );
---
+++ my-mixo/src/components/Modale/Modale.js
@@ -0,0 +1,7 @@
+import styles from './Modale.module.css'
+
+export default function Modale() {
+    return <div className={styles.modaleButton} >
+        <span className={styles.menuText} >Menu</span>
+    </div>
+}
