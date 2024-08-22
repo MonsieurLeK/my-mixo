6366f502efdcac92574ac00cda945c1260326a71
Benjamin Gauvin <benjamingauvin.dev@gmail.com>
2024-08-22 13:21:19 +0200

    Version mobile terminee

--- my-mixo/src/App.js
+++ my-mixo/src/App.js
@@ -9,6 +9,14 @@ function App() {
     <div className="App">
       <Header/>
       <Modale />
+      <p className='myMixoConcept' >Chez My Mixo, nous transformons l'art du cocktail en une expérience 
+        simple et accessible. <br/>Professionnel ou bien particulier amateur de bons moments? <br/>
+         Nos pre-mix de cocktails en bouteilles de 3 litres sont conçus pour réaliser jusqu'à 25 
+        cocktails alcoolisés (12 cl par verre) ou 18 cocktails sans alcool 
+        (16 cl par verre). <br/>Envie d'une création unique ?<br/> Nous proposons également 
+        des cocktails sur-mesure adaptés à vos goûts et vos envies. 
+        <br/>Bénéficiez également de cocktails personnalisés sur demande.<br/><br/>
+        <span className='slogan' >Avec My Mixo, savourez la perfection à chaque verre sans effort.</span></p>
       <Gallery />
     </div>
   );
--- my-mixo/src/components/Modale/Modale.js
+++ my-mixo/src/components/Modale/Modale.js
@@ -1,7 +1,26 @@
 import styles from './Modale.module.css'
+import { useState } from 'react'
+import menuPic from '../../assets/menuseul.webp'
 
 export default function Modale() {
-    return <div className={styles.modaleButton} >
-        <span className={styles.menuText} >Menu</span>
+
+    const [isVisible, setVisible] = useState(false)
+
+    function makeVisible() {
+        setVisible(true)
+    }
+    function makeDisappear() {
+        setVisible(false)
+    }
+
+    return <> <div className={styles.modaleButton} 
+    onClick={isVisible ? makeDisappear : makeVisible} >
+        <span className={isVisible ? styles.menuTextLeft : styles.menuText} >Menu 
+            <span className={styles.menuCross} >✖</span> 
+            </span>
     </div>
+    <div className={isVisible ? styles.menuContainer : styles.menuHidden} >
+        <img src={menuPic} alt='menu des cocktails' className={styles.menuPicture} ></img>
+    </div>
+    </>
 }
